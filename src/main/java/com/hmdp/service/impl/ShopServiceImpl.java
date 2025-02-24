6e54af552b4346ef66a7cc1ba85b4b1cfd2567b5
Yan6527 <101031237+Yan6527@users.noreply.github.com>
2025-02-24 20:30:06 +0800

    redis-封装redis工具包

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -8,6 +8,7 @@ import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.CacheClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,9 +32,8 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Resource
     public StringRedisTemplate stringRedisTemplate;
-
-
-
+    @Resource
+    public CacheClient cacheClient;
 
 
     /**
@@ -43,106 +43,15 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      * @return
      */
     public Result queryById(Long id) {
-        //缓存穿透
-        //Shop shop = queryWithPassThrough(id);
-        //互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+
+        //调用工具类解决缓存击穿
+        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
         return Result.ok(shop);
     }
 
-
-    public Shop queryWithPassThrough(Long id){
-        String key = CACHE_SHOP_KEY + id;
-        //1.从Redis查询缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(key);
-        //2.判断是否存在
-        if(StrUtil.isNotBlank(shopJson)){
-            //3.存在，直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return shop;
-        }
-        //上面是有值的情况，下面是无值的2种情况：A：空字符串。B：null。
-        if(shopJson != null){
-            return null;
-        }
-        //4.不存在，根据id查询数据库
-        Shop shop = getById(id);
-        //5.不存在，返回错误
-        if(shop==null){
-            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
-            return null;
-        }
-        //6.存在，写入Redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        return shop;
-    }
-    /**
-     * 获取锁
-     *
-     * @param key
-     * @return
-     */
-    public boolean tryLock(String key){
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
-        return BooleanUtil.isTrue(flag);
-    }
-
-    /**
-     * 释放锁
-     *
-     * @param key
-     */
-    public void unLock(String key){
-        stringRedisTemplate.delete(key);
-    }
-
-    public Shop queryWithMutex(Long id){
-        String key = CACHE_SHOP_KEY + id;
-        //1.从Redis查询缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(key);
-        //2.判断是否存在
-        if(StrUtil.isNotBlank(shopJson)){
-            //3.存在，直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return shop;
-        }
-        //上面是有值的情况，下面是无值的2种情况：A：空字符串。B：null。
-        if(shopJson != null){
-            return null;
-        }
-        //4.实现缓存重建
-        //4.1 获取互斥锁
-        String lockKey = LOCK_SHOP_KEY+id;
-        Shop shop = null;
-        try {
-            boolean isLock = tryLock(lockKey);
-            //4.2 判断是否获取成功
-            if(!isLock){
-                //4.3 失败，则休眠并重试
-                Thread.sleep(50);
-                return queryWithMutex(id);
-            }
-
-            //4.4 获取互斥锁成功，根据id查询数据库
-            shop = getById(id);
-            //模拟重建的延时
-            Thread.sleep(200);
-            //5.数据库查询失败，返回错误
-            if(shop==null){
-                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
-                return null;
-            }
-            //6.存在，写入Redis
-            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }finally {
-            //7.释放互斥锁
-            unLock(lockKey);
-        }
-        //8.返回
-        return shop;
-    }
     @Transactional
     @Override
     public Result updateShop(Shop shop) {
